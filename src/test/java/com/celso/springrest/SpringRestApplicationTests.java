5b054e160e948a518fcc2e44444fdf333b64ed16
Celso Bertolotto <celsonyl@hotmail.com>
2021-10-08 13:56:22 -0300

    first commit

---
+++ src/test/java/com/celso/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celso.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
