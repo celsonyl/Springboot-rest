898837bb8e8bcc9c868c45b53ea5442c068abe27
Celso Bertolotto <celsonyl@hotmail.com>
2021-10-19 16:22:01 -0300

    add flyway to populate and create table db

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`, `birth_day`)
+VALUES (1, 'AAAA', 'Ca', 'AAAAA', 'BO', NULL),
+       (5, 'sla', 'Celso', 'Masculino', 'Bertolotto', NULL),
+       (6, 'sla', 'Celso', 'Masculino', 'Bertolotto', '2000-04-03 00:00:00'),
+       (7, 'sla', 'Celso', 'Masculino', 'Bertolotto', NULL);
