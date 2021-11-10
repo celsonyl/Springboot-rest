716fa2343b5de2732d2ce7ae4054a25408348d2e
Celso Bertolotto <celsonyl@hotmail.com>
2021-11-10 13:38:05 -0300

    implementing SecurityConfig

---
+++ src/main/java/com/celso/springrest/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.celso.springrest.config;
+
+import com.celso.springrest.security.jwt.JwtConfigurer;
+import com.celso.springrest.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/auth/signing", "/api-docs/", "swagger-ui.html**").permitAll()
+                    .antMatchers("/api/**").authenticated()
+                    .antMatchers("/users").denyAll()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+}
