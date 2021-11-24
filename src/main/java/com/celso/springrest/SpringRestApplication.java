c5a356af5296d2c04ebb11d8344aa21d99bbfa52
Celso Bertolotto <celsonyl@hotmail.com>
2021-11-24 16:00:20 -0300

    WIP: upload and download files

--- src/main/java/com/celso/springrest/SpringRestApplication.java
+++ src/main/java/com/celso/springrest/SpringRestApplication.java
@@ -1,9 +1,14 @@
 package com.celso.springrest;
 
+import com.celso.springrest.config.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageConfig.class
+})
 public class SpringRestApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringRestApplication.class, args);
---
+++ src/main/java/com/celso/springrest/services/FileStorageService.java
@@ -0,0 +1,49 @@
+package com.celso.springrest.services;
+
+import com.celso.springrest.config.FileStorageConfig;
+import com.celso.springrest.exceptions.handler.FileStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir())
+                .toAbsolutePath()
+                .normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new FileStorageException("Could not create directory where uploaded files will be storage", ex);
+        }
+    }
+
+    public String storageFile(MultipartFile multipartFile) {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! filename contains invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(multipartFile.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        } catch (Exception ex) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again", ex);
+        }
+    }
+}
